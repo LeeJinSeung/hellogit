2c2037694803667dc11c1a90754d211734d18dbb
jinseung <wlstmd531@gmail.com>
2020-07-04 17:23:08 +0900

    add console log

--- hello.js
+++ hello.js
@@ -1,17 +1,17 @@
 function add(a,b) {
     var ret = a+b;
     console.log("add result : " + ret);
-    return a+b
+    return ret
 }
 
 function sub(a,b) {
     var ret = a-b;
     console.log("sub result : " + ret);
-    return a-b
+    return ret
 }
 
 function mul(a,b) {
     var ret = a*b;
     console.log("mul result : " + ret);
-    return a*b
+    return ret
 }
